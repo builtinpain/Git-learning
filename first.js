3c4484ec278604808521e446d45794afec754f02
Aryan <Shrivastavaryan1632gmail.com>
2025-09-13 17:10:16 +0530

    upi done.

--- first.js
+++ first.js
@@ -4,11 +4,13 @@ console.log(button);
 const login ="Login page added";
 console.log(login);
 
-<<<<<<< HEAD
+
 const footer ="footer page added";
 console.log(footer);
 
-=======
 const payment="Integrated the payment gateway";
 console.log("payment");
->>>>>>> 66464c9 (payment added)
+
+
+const upi="Integrated the UPI";
+console.log(upi);
