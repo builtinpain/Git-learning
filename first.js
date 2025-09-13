f665835f2348918da3a569b45c58e789361120b9
Aryan <Shrivastavaryan1632gmail.com>
2025-09-13 17:08:15 +0530

    payment added

--- first.js
+++ first.js
@@ -4,6 +4,11 @@ console.log(button);
 const login ="Login page added";
 console.log(login);
 
+<<<<<<< HEAD
 const footer ="footer page added";
 console.log(footer);
 
+=======
+const payment="Integrated the payment gateway";
+console.log("payment");
+>>>>>>> 66464c9 (payment added)
